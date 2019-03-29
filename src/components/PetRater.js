5a25401acf24b77f835f5667a52bc8e1d0df5a74
Raif Harik <reharik@gmail.com>
2019-03-29 11:15:46 -0500

    cleaning up

--- src/components/PetRater.js
+++ src/components/PetRater.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/petRater.css';
 
 const PetRater = ({imageUrls}) => {
-  return (<div className={"petRater"}>Code goes here</div>)
+  return (<div className={"PetRater"}>Code goes here</div>)
 };
 
 export { PetRater };
