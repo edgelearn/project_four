44ff6d8987e969a1c16faffd87b79c96e1f84c28
Raif Harik <reharik@gmail.com>
2019-03-29 12:36:16 -0500

    changing the app comp from class to function

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import { PetRater } from './PetRater';
-import imageUrls from '../imageUrls.json';
-import '../css/app.css';
+import React from "react";
+import { PetRater } from "./PetRater";
+import imageUrls from "../imageUrls.json";
+import "../css/app.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <PetRater imageUrls={imageUrls} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <PetRater imageUrls={imageUrls} />
+    </div>
+  );
+};
 
 export default App;
